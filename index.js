c0a96faf2ecd4e300f21706e856760f0b8c9622f
Nilesh Awachat <nileshawachat11@gmail.com>
2025-05-09 13:01:45 +0530

    Createing file/image upload project Till server created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 8002
+
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    return res.render('homepage')
+})
+
+app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
