63cb07173d17e2ef2f0d1a492c4f94ed91d9c534
Nilesh Awachat <nileshawachat11@gmail.com>
2025-05-09 13:44:59 +0530

    multer use for handling multipart/form-data

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const path = require('path')
+const multer = require('multer')
 
 const app = express()
 const PORT = 8002
 
 app.set('view engine','ejs')
 app.set('views',path.resolve('./views'))
+const upload = multer({
+    dest:'uploads/'
+})
 
-app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
     return res.render('homepage')
+})
+
+app.post('/upload',upload.single('profileimage'),(req,res)=>{
+console.log(req.body)
+console.log(req.file)
+
+return res.redirect('/');
 })
 
 app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
